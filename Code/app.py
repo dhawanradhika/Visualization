9908bc0dca44e7d0c3ec429dcb088f0a6c541f72
Radhika Dhawan <32319235+dhawanradhika@users.noreply.github.com>
2019-04-30 17:54:49 -0400

    Add files via upload

---
+++ Code/app.py
@@ -0,0 +1,73 @@
+"""
+This file is part of the flask+d3 Hello World project.
+"""
+import numpy as np
+import sys
+import itertools
+import time
+from pathlib import Path
+import pandas as pd
+import json
+import flask
+
+
+#-------------------------------------------------------------------------
+# Setting paths for data files
+#base_path="./data/"
+#song_data=base_path+"songs.csv"
+#-------------------------------------------------------------------------
+# Loading Data Files
+#df = pd.read_csv(song_data)
+#columns = df.columns
+#df.columns = ['attr-'+str(i) for i in range(1,len(df.columns)+1)]
+#-------------------------------------------------------------------------
+#Utility Functions
+
+#Get the trend of a word for line chart
+def getWordTrend(word):
+    print("----------------------------------------"+word+"------------------------------------------");
+
+# ------------------------------------------------------------------
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    When you request the root path, you'll get the index.html template.
+
+    """
+    return flask.render_template("index.html")
+
+@app.route("/word_cloud")
+def scatter():
+    return flask.render_template("word_cloud.html")
+
+@app.route("/line")
+def line():
+    return flask.render_template("line_chart.html")
+
+@app.route("/data")
+@app.route("/data/<string:chartName>")
+def data(chartName=""):
+    print("---------------------------"+chartName+"---------------------")
+    if os.path.exists("data/"+chartName+"_count.json"):
+        with open("data/"+chartName+"_count.json", 'r') as infile:
+            data = json.load(infile)
+    else:
+        line_data = getWordTrend(word)
+        with open(chartName+'_count.json', 'w') as outfile:
+            json.dump(data, outfile)
+    return json.dumps(data)
+
+if __name__ == "__main__":
+    import os
+
+    port = 8000
+
+    # Open a web browser pointing at the app.
+    os.system("open http://localhost:{0}".format(port))
+
+    # Set up the development server on port 8000.
+    app.debug = True
+    app.run(port=port)
